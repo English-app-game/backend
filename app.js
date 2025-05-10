d24ddc25e687e3664ce04311292df35932eb5412
maymichaeli <maushe55@gmail.com>
2025-05-10 19:10:43 +0300

    add login route and login API

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { CLIENT_URL, ROOMS_ROUTE } from "./config/consts.js";
 import userRouter from "./routes/User-route.js";
 import { USERS_ROUTE } from "./config/consts.js";
 import { router as roomsRouter } from "./routes/rooms/router.js";
+import loginRoute from "./routes/login.js";
 
 export const initApp = () => {
   const app = express();
@@ -18,6 +19,8 @@ export const initApp = () => {
   app.use(express.json());
   app.use(USERS_ROUTE, userRouter);
   app.use(ROOMS_ROUTE, roomsRouter);
+  app.use(loginRoute);
+
 
   return app;
 };
