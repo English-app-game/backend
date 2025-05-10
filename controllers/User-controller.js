75db3d240481ee98f26f0e617073e1b1d8595a3c
LiadLati <liadl57@gmail.com>
2025-05-10 15:11:56 +0300

    user registration functions

--- controllers/User-controller.js
+++ controllers/User-controller.js
@@ -1,10 +1,10 @@
-import register from "../services/User-service.js";
-import {UserModel} from "../models/User.js";
+import UserService from "../services/user-service";
+import UserModel from "../models/User";
 
 const registerController = async (req,res) => {
     try{
         const user = new UserModel(req.body);
-        const savedUser = await register(user);
+        const savedUser = await UserService.register(user);
         res.status(201).json(savedUser);
     } catch (error) {
         res.status(400).json({message:error.message});
