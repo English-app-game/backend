95a9402d9bbd1e722584f85bdf0950a27e9d04ec
LiadLati <liadl57@gmail.com>
2025-05-11 01:48:34 +0300

    userRegistration back complete

--- controllers/User-controller.js
+++ controllers/User-controller.js
@@ -1,10 +1,10 @@
-import UserService from "../services/user-service";
-import UserModel from "../models/User";
+import register from "../services/User-service.js";
+import {UserModel} from "../models/User.js";
 
 const registerController = async (req,res) => {
     try{
         const user = new UserModel(req.body);
-        const savedUser = await UserService.register(user);
+        const savedUser = await register(user);
         res.status(201).json(savedUser);
     } catch (error) {
         res.status(400).json({message:error.message});
--- routes/User-route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userController from '../controllers/user-controller';
-
-const router = express.Router();
-
-router.route('register').post(userController.registerController);
-
-export default router;
