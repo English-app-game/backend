800526ff55702cdf67f368667b2e4a674c54898a
maymichaeli <maushe55@gmail.com>
2025-05-17 16:04:15 +0300

    change login logic to controller

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/User.js";
+import { createToken } from "../utils/jwt.js";
+
+export async function loginUserController(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    const token = createToken({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      avatarImg: user.avatarImg,
+      isGuest: false,
+    });
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatarImg: user.avatarImg,
+      },
+    });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
