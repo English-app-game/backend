dffe61e56d25fa2893e304e30a41e6e27c64d507
maymichaeli <maushe55@gmail.com>
2025-05-17 16:18:49 +0300

    fix syntax bugs

--- routes/guest.js
+++ routes/guest.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import { createToken } from "../utils/jwt.js";
-import { GUEST_ROUTE } from "../constants/apiRoutes.js";
+import { GUEST_ROUTE } from "../routes/routes_consts.js";
 
 
 const router = express.Router();
