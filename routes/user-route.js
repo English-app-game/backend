66dfc8059eae931873d16c8e3b00640584f14a25
LiadLati <liadl57@gmail.com>
2025-05-19 17:40:09 +0300

    forgetPassword-2.4 save changes

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import registerController from '../controllers/user-controller.js';
-import { REGISTER_ROUTE } from '../config/consts.js';
+import { REGISTER_ROUTE, RESET_PASSWORD_ROUTE } from '../config/consts.js';
+import sendResetPasswordEmail from "../controllers/ResetPasswordController.js";
 
 const router = express.Router();
 
 router.post(REGISTER_ROUTE,registerController);
+router.post(RESET_PASSWORD_ROUTE, sendResetPasswordEmail);
+
 
 export default router;
