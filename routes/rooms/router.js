494b7acf46acaed5527ebc2aa002a87edaab5a83
tomergol15 <golod505@gmail.com>
2025-05-23 19:29:41 +0300

    ADD: function to the controller and add the route for her

--- routes/rooms/router.js
+++ routes/rooms/router.js
@@ -8,6 +8,8 @@ const ROOM_ROUTE_BY_KEY = "/check/:key";
 router.route("/create").post(roomController.addRoomToDB);
 router.route("/").get(roomController.getRooms);
 router.route(ROOM_ROUTE_BY_KEY).get(roomController.checkRoomAvailabilityByKey);
+router.route("/players/:key").get(roomController.getRoomWithPlayers);
+
 
 
 export { router, ROOM_ROUTE_BY_KEY };
