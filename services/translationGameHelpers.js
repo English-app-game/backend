cda4e53091018c2a699a0df16416b9c7668dde1c
LaNguAx <itayakni@gmail.com>
2025-06-27 02:01:15 +0300

    bugfix/translation-game-unique-player-colors

--- services/translationGameHelpers.js
+++ services/translationGameHelpers.js
@@ -1,4 +1,4 @@
-const playerColors = ["#fde047", "#fca5a5", "#ddd6fe", "#86efac", "#bae6fd", "#e5e7eb", "#67e8f9"];
+const playerColors = ["#ff6b6b", "#4ecdc4", "#45b7d1", "#96ceb4", "#ffeaa7", "#dda0dd", "#ff8c42"];
 
 export const generateRandomColor = (usedColors = []) => {
   const availableColors = playerColors.filter((c) => !usedColors.includes(c));
